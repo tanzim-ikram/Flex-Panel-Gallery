28e7bf0d522b9afb2bdac60219f7920d30f089b5
Tanzim Ikram Sheikh <tanzim.ikram@gmail.com>
2025-03-06 13:44:41 +0600

    Add script to make the panel functional

---
+++ script.js
@@ -0,0 +1,15 @@
+const panels = document.querySelectorAll(".panel");
+console.log(panels);
+
+function toggleOpen() {
+    this.classList.toggle("open");
+}
+
+function toggleActive(e) {
+    if (e.propertyName === 'flex-grow') {
+        this.classList.toggle("open-active");
+    }
+}
+
+panels.forEach(panel => panel.addEventListener("click", toggleOpen));
+panels.forEach(panel => panel.addEventListener("transitionend", toggleActive));
